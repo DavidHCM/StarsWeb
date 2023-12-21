737b86c3832f0758a98b0fbea388ddea5994d7ae
David Hernández <david.hernandezc@iteso.mx>
2023-12-20 19:51:00 -0600

    installing more requirements

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const Swal = require('sweetalert2')
 
 const app = express();
 const port = process.env.PORT ||  3000;
-
 app.use(express.json())
+
+app.use(express.static('./src'));
+app.use('/styles', express.static('styles'))
+app.use('/scripts', express.static('scripts'))
+app.use('/assets', express.static('assets'))
+app.use('/controllers', express.static('controllers'))
 
 app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
+
+const router = require('./routes/router.js');
+
+app.use('/', router);
 
 app.listen(port, () =>
 {
