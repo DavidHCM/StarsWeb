a328d7e6f4bc3a2835cd473ce1b559a0c83f7d02
David Hernández <david.hernandezc@iteso.mx>
2023-12-20 22:05:57 -0600

    changing structure

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+
+
+
+
 router.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/../src/views/mainPage.html"));
 })
